1fa4095f53440c4cdbf67c1742ed9cf7af73e71c
Dimitris Mouris <jimouris@gmail.com>
2017-08-27 01:38:42 +0300

    Add comments to Private Tak function

--- tak_function/tak_unrolled.c
+++ tak_function/tak_unrolled.c
@@ -4,6 +4,7 @@
 #define MAX_NUM 8
 #define ITERATIONS MAX_NUM-1
 
+// Textbook Version
 int tak(int x, int y, int z) {
     while (x > y) {
         int oldx = x, oldy = y;
@@ -16,21 +17,21 @@ int tak(int x, int y, int z) {
     return z;
 }
 
+//--Private Tak function--//
+
 int gfun(int x, int y) {
     return (x <= 0) ? 0 : y;
 }
 
-/*
- * Variable "iter" will not be encrypted
- */
+// Variable "iter" will not be encrypted
 int tak_unrolled(int x, int y, int z, int iter) {
-    int sel = gfun(x-y, 1);     // sel = x > y;
-    while (iter--) {
-        int oldx = (1 - sel) * oldx + sel * x;
+    int sel = gfun(x-y, 1);                                                 // sel = x > y;
+    while (iter--) {                                                        // perform fixed iterations
+        int oldx = (1 - sel) * oldx + sel * x;                              // update oldx depending on the value of sel
         int oldy = (1 - sel) * oldy + sel * y;
-        x = (1 - sel) * x + sel * tak_unrolled(x - 1, y, z, iter);
+        x = (1 - sel) * x + sel * tak_unrolled(x - 1, y, z, iter);          // keep either x or the result of recursion
         y = (1 - sel) * y + sel * tak_unrolled(y - 1, z, oldx, iter);
-        sel = gfun(x-y, 1);     // sel = x > y;
+        sel = gfun(x-y, 1);                                                 // update sel = x > y;
         z = (1 - sel) * z + sel * tak_unrolled(z - 1, oldx, oldy, iter);
     }
     return z;
@@ -42,7 +43,7 @@ int main(void) {
             for (int k = 0 ; k <= MAX_NUM ; k++) {
                 int x = tak(i, j, k);
                 printf("%d %d %d : %d\n", i, j, k, x);
-                assert(x == tak_unrolled(i, j, k, ITERATIONS));
+                assert(x == tak_unrolled(i, j, k, ITERATIONS)); // check that private and textbook tak functions have the same results
             }
         }
     }
