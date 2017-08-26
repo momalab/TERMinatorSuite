0dc672db6f37c969d2efcbe6721f9e488aea9557
Dimitris Mouris <jimouris@gmail.com>
2017-08-27 01:34:28 +0300

    Add comments to InsertionSort

--- insertionSort/insertionSort.c
+++ insertionSort/insertionSort.c
@@ -1,15 +1,6 @@
 /* insertion sort ascending order */
 #include <stdio.h>
  
-// swap if the second is greater
-void minswp(int *x, int *y) {
-    if (*x < *y) {
-        int tmp = *x;
-        *x = *y;
-        *y = tmp;
-    }
-}
-
 int gfun(int x, int y) {
     return (x <= 0) ? 0 : y;
 }
@@ -17,7 +8,7 @@ int gfun(int x, int y) {
 int main(void) {
     int array[10] = { 4, 2, 5, 9, 1, 0, 4, 3, 9, 8 };
 
-    int N = sizeof(array) / sizeof(array[0]);
+    int N = sizeof(array) / sizeof(array[0]);               // get size of array
     int i, j;
     for (i = 1 ; i < N; i++) {
         j = i;
@@ -25,12 +16,13 @@ int main(void) {
             int x = array[j-1];
             int y = array[j];
 
-            int diff = gfun(x-y, x-y) + gfun(y-x, y-x);
-            int max = gfun(x-y, x) + gfun(y-x, y);
-            max += (x == y) * x; // if x == y set max to x
-            int min = max - diff;
+            int diff = gfun(x-y, x-y) + gfun(y-x, y-x);     // compute |x-y|
+            int max = gfun(x-y, x) + gfun(y-x, y);          // compute max(x, y)
+            max += (x == y) * x;                            // if x == y set max to x
+            int min = max - diff;                           // compute min 
 
-            array[j-1] = min;
+            // swap in ascending order 
+            array[j-1] = min;                               
             array[j] = max;
             j--;
         }
